a91264536484363ff643eb35b869cbdddab464ff
Fariz <farizalfairuz27@gmail.com>
2022-01-25 06:46:02 +0700

    add: post details page

---
+++ src/pages/PostDetails/PostDetailsPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function PostDetailsPage() {
+    const params = useParams()
+    
+    return (
+        <div>
+            testing
+        </div>
+    )
+}
+
+export default PostDetailsPage
