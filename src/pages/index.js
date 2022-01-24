120fe8b62c03915d27642e19c1ef450a5317e8a5
Fariz <farizalfairuz27@gmail.com>
2022-01-25 06:46:47 +0700

    add: pages export

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export {default as Home} from './Home'
+export {default as PhotoDetails} from './PhotoDetails'
+export {default as PostDetails} from './PostDetails'
+export {default as UserDetails} from './UserDetails'
