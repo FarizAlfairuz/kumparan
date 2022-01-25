7e9189db568c9d3eeadb1a5a09e8e9c20280dd78
Fariz <farizalfairuz27@gmail.com>
2022-01-25 09:35:44 +0700

    add: album api

--- src/api/PhotosAPI.js
+++ src/api/PhotosAPI.js
@@ -5,11 +5,13 @@ const PhotosAPI = {
         return API.get('/photos')
     },
 
+    getUserAlbums(id) {
+        return API.get(`/users/${id}/album`)
+    },
+
     getSingleAlbum(id) {
         return API.get(`/album/${id}/photos`)
     },
-
-    
 }
 
 export default PhotosAPI
