f98d6330d40de94f9b40c9bc013d5b9cc86e415d
Fariz <farizalfairuz27@gmail.com>
2022-01-25 10:37:56 +0700

    add: truncate list

--- src/components/cards/AlbumCards.js
+++ src/components/cards/AlbumCards.js
@@ -1,15 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useAlbum from "@hooks/useAlbum";
+import TruncatedList from "react-truncate-list";
 
 function AlbumCards(props) {
   const { id } = props;
   const { album, getSingleAlbum } = useAlbum();
+  const [expanded, setExpanded] = useState(false);
+  const expand = () => {
+    console.log(id + 'expand' + expanded)
+    setExpanded(true)
+  };
+  const collapse = () => setExpanded(false);
+
   useEffect(() => {
     getSingleAlbum(id);
 
   }, [getSingleAlbum, id]);
 
-  console.log(album)
 
   return album.loading ? (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
@@ -18,14 +25,32 @@ function AlbumCards(props) {
       <div className="h-20 bg-gray-50 rounded-sm animate-pulse"></div>
     </div>
   ) : (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+    <TruncatedList 
+      className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 max-h-72 ${expanded ? "max-h-full" : ""}`}
+      alwaysShowTruncator
+      renderTruncator={({ hiddenItemsCount }) => {
+        if(hiddenItemsCount > 0) {
+          return (
+            <button className="text-white bg-blue-400 px-3 py-2 rounded" onClick={expand}>
+              {hiddenItemsCount} more...
+            </button>
+          );
+        } else {
+          return (
+            <button className="text-white bg-blue-400 px-3 py-2 rounded" onClick={collapse}>
+              hide
+            </button>
+          );
+        }
+      }}
+      >
       {album.data.map((single, index) => (
         <div key={index}>
           <img src={single.thumbnailUrl} alt={single.title} />
           <p className="text-sm line-clamp-2">{single.title}</p>
         </div>
       ))}
-    </div>
+    </TruncatedList>
   );
 }
 
